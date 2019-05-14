32f9c8aec97d8835d0bbeca0d7b6c5f39dd31a11
Jake Taylor <jake.taylor.se@gmail.com>
2019-05-14 12:32:32 -0400

    New js files

---
+++ Migrations/201905140303100_AddConstraints.cs
@@ -0,0 +1,16 @@
+namespace JTicket.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddConstraints : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ Views/ViewModels/NewTicketViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JTicket.Models;
+
+namespace JTicket.Views.ViewModels
+{
+    public class TicketFormViewModel
+    {
+        public IEnumerable<Severity> severities { get; set; }
+        public Ticket Ticket { get; set; }
+    }
+}
