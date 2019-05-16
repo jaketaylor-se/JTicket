1b43b63c9e9f3909037af8c1184a8b94fd4dded8
Jake Taylor <jake.taylor.se@gmail.com>
2019-05-16 15:34:27 -0400

    Built the intial Ticket Web API to be consumed

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using AutoMapper;
+using JTicket.App_Start;
+
 
 namespace JTicket
 {
@@ -12,6 +16,9 @@ namespace JTicket
     {
         protected void Application_Start()
         {
+            // Add mapping profile to global config
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
