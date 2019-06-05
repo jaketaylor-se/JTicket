ed1a9f4b1aa0092755aff3fb3d22ffacfcfb86db
Jake Taylor <jake.taylor.se@gmail.com>
2019-06-04 20:18:55 -0400

    Refactored the Bundle Configuration

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -1,14 +1,21 @@
-﻿using System.Web;
+﻿/* 
+ *  Bundle Configurations.
+ */
+
 using System.Web.Optimization;
 
 namespace JTicket
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        /* For more information on bundling, 
+        visit https://go.microsoft.com/fwlink/?LinkId=301862 */
         public static void RegisterBundles(BundleCollection bundles)
         {
 
+            /* Create a larger bundle with third-party javaScript files.
+               The goal is to minimize the amount of HTTP requests needed
+               to get the javaScript files. */
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
@@ -22,13 +29,17 @@ namespace JTicket
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            // Use the development version of Modernizr to develop with and 
+            // learn from. Then, when you're ready for production, use the 
+            // build tool at https://modernizr.com to pick only the tests you 
+            // need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            /* Bundle containing CSS files. The Bootstrap template is 
+             * bootstrap-lumen.css. */ 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap-lumen.css",  // Bootstrap templace    
                       "~/Content/Site.css",
                       "~/Content/datatables/css/datatables.bootstrap4.css"));
         }
