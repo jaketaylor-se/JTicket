54bb9371f7104d0a627a57e94c1730446010890b
Jake Taylor <jake.taylor.se@gmail.com>
2019-06-04 21:25:31 -0400

    Refactored Route configurations

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,22 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿/*
+ *  Route Configurations. 
+ */
+
 using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace JTicket
 {
+    /// <summary>
+    /// Class 
+    /// <c>RouteConfig</c> 
+    /// Route Configuration class. 
+    /// </summary>
     public class RouteConfig
     {
+        /// <summary>
+        /// Method 
+        /// <c>RegisterRoutes</c> 
+        /// Register the routes. Method is called upon application start.
+        /// </summary>
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute    // Default route
+            (
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new
+                {
+                  controller = "Home",          // Default to Home Controller
+                  action = "Index",
+                  id = UrlParameter.Optional    // Id is optional
+                }
             );
         }
     }
