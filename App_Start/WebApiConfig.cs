e2a510223be00cbd23df13c6c442a9e6c576e298
Jake Taylor <jake.taylor.se@gmail.com>
2019-06-04 21:46:40 -0400

    Refactored the configurations for the web api

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,27 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿/*
+ *  Configurations for the JTicket RESTful Web API.
+ */
+
 using System.Web.Http;
 using Newtonsoft.Json.Serialization;
 
 namespace JTicket
 {
+    /// <summary>
+    /// Class 
+    /// <c>WebApiConfig</c> 
+    /// Configuration for the RESTful Web API for JTicket.
+    /// </summary>
     public static class WebApiConfig
     {
+        /// <summary>
+        /// Method 
+        /// <c>Register</c> 
+        /// Register HTTP routes for Web API.
+        /// </summary>
         public static void Register(HttpConfiguration config)
         {
-            // Formatting settings. The goal is to make the JSON returned from the
-            // Web API more digestable for JavaScript.
+            // Formatting settings. The goal is to make the JSON 
+            // returned from the Web API more digestable for JavaScript.
             var settings = config.Formatters.JsonFormatter.SerializerSettings;
-            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.ContractResolver =    // Format to Camel Case 
+                new CamelCasePropertyNamesContractResolver();
             settings.Formatting = Newtonsoft.Json.Formatting.Indented;
 
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
+            config.Routes.MapHttpRoute
+            (
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new
+                {
+                    id = RouteParameter.Optional
+                }
             );
         }
     }
