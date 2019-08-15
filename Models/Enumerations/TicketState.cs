b78fa1134332e6299183c5b3ec86542c7c71fff2
Jake Taylor <jake.taylor.se@gmail.com>
2019-08-15 13:19:15 -0400

    Commiting additional files needed for Kanban board

---
+++ Models/Enumerations/TicketState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JTicket.Models.Enumerations
+{
+    public enum TicketState
+    {
+        open = 0,
+        analysis = 1,
+        debugging = 2,
+        testing = 3,
+        resolved = 4
+    }
+}
---
+++ TicketSeed.sql
@@ -0,0 +1,185 @@
+﻿
+DELETE FROM Tickets;
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 1',
+        'There are example comments for test ticket 1',
+		1,
+		4,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 1',
+		0);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 2',
+        'There are example comments for test ticket 2',
+		0,
+		2,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 2',
+		4);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 3',
+        'There are example comments for test ticket 3',
+		1,
+		5,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 3',
+		2);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 4',
+        'There are example comments for test ticket 4',
+		1,
+		1,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 4',
+		3);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 5',
+        'There are example comments for test ticket 5',
+		0,
+		5,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 5',
+		4);
+
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 6',
+        'There are example comments for test ticket 6',
+		0,
+		3,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 6',
+		4);
+
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 7',
+        'There are example comments for test ticket 7',
+		1,
+		5,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 7',
+		2);
+
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 8',
+        'There are example comments for test ticket 8',
+		1,
+		5,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 8',
+		2);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 9',
+        'There are example comments for test ticket 9',
+		1,
+		3,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 9',
+		3);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 10',
+        'There are example comments for test ticket 10',
+		0,
+		2,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 10',
+		4);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 11',
+        'There are example comments for test ticket 11',
+		0,
+		5,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 11',
+		4);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 12',
+        'There are example comments for test ticket 12',
+		1,
+		2,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 12',
+		1);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 13',
+        'There are example comments for test ticket 13',
+		1,
+		4,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 1',
+		1);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 14',
+        'There are example comments for test ticket 14',
+		1,
+		2,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 14',
+		1);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 15',
+        'There are example comments for test ticket 15',
+		1,
+		5,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 15',
+		2);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 16',
+        'There are example comments for test ticket 16',
+		1,
+		4,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 16',
+		2);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 17',
+        'There are example comments for test ticket 17',
+		1,
+		2,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 17',
+		3);
+
+INSERT INTO Tickets 
+VALUES ('This is a description for test ticket 18',
+        'There are example comments for test ticket 18',
+		1,
+		1,
+		CURRENT_TIMESTAMP,
+		CURRENT_TIMESTAMP,
+		'Test Ticket 1',
+		3);
